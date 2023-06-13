b858adbd8f9fa26c31e016b4a040ba2ca5360ec3
OverTimeChaos <jamesfan2006@hotmail.com>
2023-06-14 08:57:45 +1000

    Fixed the F string and loaded json

--- Maingame.py
+++ Maingame.py
@@ -1,20 +1,24 @@
+import json
+question = json.load(open("topic.json", "r"))
 def startgame():
+    global question
     Start_game = input ("would you like to play the quiz? Enter T for tutorial ")
     if Start_game.lower() == "y":
         print ("starting game")
     elif Start_game.lower() =="t":
-        print (f'Welcome to Gamivia')
+        print(f'''Welcome to Gamivia
+A random topic will be chosen
+You will have to guess that topic
+Fortunately there are clues for these topics
+Unfortunately each clue cost one 1 point
+Clues range from 1 to 20
+Enter the corresponding number to get the corresponding clue
+When you are ready to guess, enter Y
+But remember each guess costs 2 points
+If you run out of points you fail the topic
+You continue guessing until all topics run out then the final score will displayed
+''')
         startgame()
-          #A random topic will be chosen
-           #You will have to guess that topic
-           #Fortunately there are clues for these topics
-           #Unfortunately each clue cost one 1 point
-           #Clues range from 1 to 20
-           #Enter the corresponding number to get the corresponding clue
-           #When you are ready to guess, enter Y
-           #But remember each guess costs 2 points
-           #If you run out of points you fail the topic
-           #You continue guessing until all topics run out then the final score will displayed
         
     else:
         print ("Then why did you open this program?")
