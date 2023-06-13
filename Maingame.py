03a30cf283530854e8f0ef8c7018084202effccb
OverTimeChaos <jamesfan2006@hotmail.com>
2023-06-14 09:18:21 +1000

    Def () shit

--- Maingame.py
+++ Maingame.py
@@ -1,8 +1,8 @@
 import json
+global question
 question = json.load(open("topic.json", "r"))
 def startgame():
-    global question
-    Start_game = input ("would you like to play the quiz? Enter T for tutorial ")
+    Start_game = input ("Would you like to play the quiz? Enter T for tutorial ")
     if Start_game.lower() == "y":
         print ("starting game")
     elif Start_game.lower() =="t":
@@ -17,10 +17,11 @@ When you are ready to guess, enter Y
 But remember each guess costs 2 points
 If you run out of points you fail the topic
 You continue guessing until all topics run out then the final score will displayed
-''')
+''')   
         startgame()
-        
     else:
         print ("Then why did you open this program?")
+       
         startgame()
+
 startgame()
