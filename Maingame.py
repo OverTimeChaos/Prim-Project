bc68a123f4ee9b86af9eebf94e34421526625917
OverTimeChaos <jamesfan2006@hotmail.com>
2023-06-15 11:24:13 +1000

    Fixed shit, and coded in and out

--- Maingame.py
+++ Maingame.py
@@ -10,7 +10,7 @@ clue_chosen = []
 
 #Allows the program to uniquely choose a Area
 def getKeyArea():
-    keyArea = list(question)[randint(0, 9)]
+    keyArea = list(question)[randint(0, len(question)-1)]
     if keyArea in completed:
         return getKeyArea()
     else:
@@ -22,6 +22,7 @@ def getclueAnswer(first=False, Clue_choose=""):
     if first:
         clue = question[topic_area][randint(0,len(topic_area)-1)]
         clue_chosen.append(clue)
+        return clue
     elif str(Clue_choose).lower() == 'y':
         guess()
     else:
@@ -31,21 +32,24 @@ def getclueAnswer(first=False, Clue_choose=""):
                 clue = question[topic_area][Clue_choose]
             except IndexError:
                 print("Value out of range")
-                return getclueAnswer(first, input("Insert Another Number or Y to take a guess "))
+                clue = getclueAnswer(first, input("Insert Another Number or Y to take a guess "))
             if clue in clue_chosen:
                 print("Clue already chosen")
-                return getclueAnswer(first, input("Insert Another Number or Y to take a guess "))
+                clue = getclueAnswer(first, input("Insert Another Number or Y to take a guess "))
             else:
                 print(clue)
                 clue_chosen.append(clue)
                 player_points = player_points-1 
         except ValueError:
             print("Invalid value!")
-            return getclueAnswer(first, input("Insert Valid Value "))
-    return clue
+            clue = getclueAnswer(first, input("Insert Valid Value "))
+        return clue
 
 def guess():
+    global player_points
     print("well you can't guess cause james is lazy and didn't code in a guess")
+    player_points = -20 
+     
     
 #Main quiz
 def game_quiz():
@@ -59,21 +63,22 @@ def game_quiz():
         topic_area = getKeyArea()
         player_points = 20
         topic_completed = False
-    while topic_completed == False:
-        if player_points == 0:
-            print (f"You have failed to guess, {topic_area}")
-            topic_completed == True
-           
-        else:
-            if firstclued == False:
-                first_clue = getclueAnswer(first=True)
-                firstclued = True
+        while not topic_completed:
+            if player_points <= 0:
+                print (f"You have failed to guess, {topic_area}")
+                topic_completed = True
+                game_score = game_score + 1
+            
             else:
-                for clue in clue_chosen:
-                    print(f" - {clue}")
-                print (f'You have {player_points} left')
-                Clue_choose = input("Would you like to guess with 'y' or get another clue? ")
-                getclueAnswer(Clue_choose=Clue_choose)
+                if firstclued == False:
+                    first_clue = getclueAnswer(first=True)
+                    firstclued = True
+                else:
+                    for clue in clue_chosen:
+                        print(f" - {clue}")
+                    print (f'You have {player_points} left')
+                    Clue_choose = input("Would you like to guess with 'y' or get another clue? ")
+                    getclueAnswer(Clue_choose=Clue_choose)
 #Gets called when the program starts
 def startgame():
     global player_points
@@ -83,7 +88,7 @@ def startgame():
     if Start_game.lower() == "y":
         print ("Starting game")
         player_points = 0
-        game_score = 0
+        game_score = 10
         player_score = 0
         game_quiz()
     elif Start_game.lower() =="t":
@@ -105,4 +110,5 @@ You continue guessing until all topics run out then the final score will display
         startgame()
 
 startgame()
-print("o")
+print (f'''final score 
+{player_score}''')
