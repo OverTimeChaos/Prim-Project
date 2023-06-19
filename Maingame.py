dbbc852a8360bd44db7e460fbb8d4b04a8fac89e
OverTimeChaos <jamesfan2006@hotmail.com>
2023-06-19 14:50:17 +1000

    player point display in guess()

--- Maingame.py
+++ Maingame.py
@@ -69,13 +69,13 @@ def getclueAnswer(first=False, Clue_choose=""):
 def checkGameOver():
     global player_points, topic_completed, game_score, clue_chosen, topic_area
     if  player_points <= 0:
-        print (f"You have failed to guess, {topic_area}")
-        topic_completed = True
-        game_score = game_score + 1
-        clue_chosen.clear()
-        topic_area = getKeyArea()
-        player_points = 20
-        game_quiz()
+                print (f"You have failed to guess, {topic_area}")
+                topic_completed = True
+                game_score = game_score + 1
+                clue_chosen.clear()
+                topic_area = getKeyArea()
+                player_points = 20
+                game_quiz()
 # allows the program to assess answers
 def guess():
     global player_points
