d4792e733ff310601931a5f2017fce1dbf721a7c
OverTimeChaos <jamesfan2006@hotmail.com>
2023-06-19 14:23:29 +1000

    Added numbers to clues in json and removed print

--- Maingame.py
+++ Maingame.py
@@ -69,13 +69,13 @@ def getclueAnswer(first=False, Clue_choose=""):
 def checkGameOver():
     global player_points, topic_completed, game_score, clue_chosen, topic_area
     if  player_points <= 0:
-                print (f"You have failed to guess, {topic_area}")
-                topic_completed = True
-                game_score = game_score + 1
-                clue_chosen.clear()
-                topic_area = getKeyArea()
-                player_points = 20
-                game_quiz()
+        print (f"You have failed to guess, {topic_area}")
+        topic_completed = True
+        game_score = game_score + 1
+        clue_chosen.clear()
+        topic_area = getKeyArea()
+        player_points = 20
+        game_quiz()
 # allows the program to assess answers
 def guess():
     global player_points
@@ -99,7 +99,6 @@ def guess():
         print ("Incorrect, two point deducted")
         player_points = player_points - 2
         checkGameOver()
-        print (player_points)
         for clue in clue_chosen:
                 print(f" - {clue}")
         clue = getclueAnswer(Clue_choose=input("Insert Another Number or Y to take a guess "))      
